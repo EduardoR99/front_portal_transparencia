4ad3769f163543c8225b8078d8a6cd771af55011
Eduardo Rodrigues <102009964+EduardoR99@users.noreply.github.com>
2025-01-27 10:03:58 -0300

    update

--- src/components/tableEmpenho.vue
+++ src/components/tableEmpenho.vue
@@ -1,61 +1,62 @@
 <template>
   <div class="overflow-x-auto">
-        <table class="min-w-full bg-white p-6 text-primaryfont">
-          <thead>
-            <tr>
+    <div class="min-w-full">
+      <table class="min-w-full text-[10px] bg-white p-6 text-primaryfont">
+        <thead>
+          <tr>
             <th class="py-2 px-4 border-b">Detalhes</th>
-              <th class="py-2 px-4 border-b">Ano</th>
-              <th class="py-2 px-4 border-b">Mês</th>
-              <th class="py-2 px-4 border-b">Unidade Gestora</th>
-              <th class="py-2 px-4 border-b">Data</th>
-              <th class="py-2 px-4 border-b">Espécie</th>
-              <th class="py-2 px-4 border-b">Empenho</th>
-              <th class="py-2 px-4 border-b">Tipo Empenho</th>
-              <th class="py-2 px-4 border-b">Elemento Despesa</th>
-              <th class="py-2 px-4 border-b">Subtítulo</th>
-              <th class="py-2 px-4 border-b">Função</th>
-              <th class="py-2 px-4 border-b">Subfunção</th>
-              <th class="py-2 px-4 border-b">Programa</th>
-              <th class="py-2 px-4 border-b">Fonte Recurso</th>
-              <th class="py-2 px-4 border-b">Grupo Despesa</th>
-              <th class="py-2 px-4 border-b">Documento Favorecido</th>
-              <th class="py-2 px-4 border-b">Nome Favorecido</th>
-              <th class="py-2 px-4 border-b">Valor</th>
-            </tr>
-          </thead>
-          <tbody>
-            <tr v-for="(item, index) in paginatedData" :key="index">
-                <td @click="goToDetails(item.id)" class="py-2 px-4 border-b text-sm cursor-pointer">Ver mais +</td>
-              <td class="py-2 px-4 border-b">{{ item.ano }}</td>
-              <td class="py-2 px-4 border-b">{{ item.mes }}</td>
-              <td class="py-2 px-4 border-b">{{ item.unidade_gestora }}</td>
-              <td class="py-2 px-4 border-b">{{ formatDate(item.data) }}</td>
-              <td class="py-2 px-4 border-b">{{ item.especie }}</td>
-              <td class="py-2 px-4 border-b">{{ item.empenho }}</td>
-              <td class="py-2 px-4 border-b">{{ item.tipo_empenho }}</td>
-              <td class="py-2 px-4 border-b">{{ item.elemento_despesa }}</td>
-              <td class="py-2 px-4 border-b">{{ item.subtitulo }}</td>
-              <td class="py-2 px-4 border-b">{{ item.funcao }}</td>
-              <td class="py-2 px-4 border-b">{{ item.subfuncao }}</td>
-              <td class="py-2 px-4 border-b">{{ item.programa }}</td>
-              <td class="py-2 px-4 border-b">{{ item.fonte_recurso }}</td>
-              <td class="py-2 px-4 border-b">{{ item.grupo_despesa }}</td>
-              <td class="py-2 px-4 border-b">{{ item.documento_favorecido }}</td>
-              <td class="py-2 px-4 border-b">{{ item.nome_favorecido }}</td>
-              <td class="py-2 px-4 border-b">{{ formatCurrency(item.valor) }}</td>
-            </tr>
-          </tbody>
-        </table>
-      </div>
-      <div class="flex justify-between mt-4">
-        <button class="bg-blue-500 text-white px-4 py-2 rounded" @click="prevPage" :disabled="currentPage === 1">
-          Anterior
-        </button>
-        <button class="bg-blue-500 text-white px-4 py-2 rounded" @click="nextPage"
-          :disabled="currentPage === totalPages">
-          Próximo
-        </button>
-      </div>
+            <th class="py-2 px-4 border-b">Ano</th>
+            <th class="py-2 px-4 border-b">Mês</th>
+            <th class="py-2 px-4 border-b">Unidade Gestora</th>
+            <th class="py-2 px-4 border-b">Data</th>
+            <th class="py-2 px-4 border-b">Espécie</th>
+            <th class="py-2 px-4 border-b">Empenho</th>
+            <th class="py-2 px-4 border-b">Tipo Empenho</th>
+            <th class="py-2 px-4 border-b">Elemento Despesa</th>
+            <th class="py-2 px-4 border-b">Subtítulo</th>
+            <th class="py-2 px-4 border-b">Função</th>
+            <th class="py-2 px-4 border-b">Subfunção</th>
+            <th class="py-2 px-4 border-b">Programa</th>
+            <th class="py-2 px-4 border-b">Fonte Recurso</th>
+            <th class="py-2 px-4 border-b">Grupo Despesa</th>
+            <th class="py-2 px-4 border-b">Documento Favorecido</th>
+            <th class="py-2 px-4 border-b">Nome Favorecido</th>
+            <th class="py-2 px-4 border-b">Valor</th>
+          </tr>
+        </thead>
+        <tbody>
+          <tr v-for="(item, index) in paginatedData" :key="index">
+            <td @click="goToDetails(item.id)" class="py-2 px-4 border-b text-sm cursor-pointer">Ver mais +</td>
+            <td class="py-2 px-4 border-b">{{ item.ano }}</td>
+            <td class="py-2 px-4 border-b">{{ item.mes }}</td>
+            <td class="py-2 px-4 border-b">{{ item.unidade_gestora }}</td>
+            <td class="py-2 px-4 border-b">{{ formatDate(item.data) }}</td>
+            <td class="py-2 px-4 border-b">{{ item.especie }}</td>
+            <td class="py-2 px-4 border-b">{{ item.empenho }}</td>
+            <td class="py-2 px-4 border-b">{{ item.tipo_empenho }}</td>
+            <td class="py-2 px-4 border-b">{{ item.elemento_despesa }}</td>
+            <td class="py-2 px-4 border-b">{{ item.subtitulo }}</td>
+            <td class="py-2 px-4 border-b">{{ item.funcao }}</td>
+            <td class="py-2 px-4 border-b">{{ item.subfuncao }}</td>
+            <td class="py-2 px-4 border-b">{{ item.programa }}</td>
+            <td class="py-2 px-4 border-b">{{ item.fonte_recurso }}</td>
+            <td class="py-2 px-4 border-b">{{ item.grupo_despesa }}</td>
+            <td class="py-2 px-4 border-b">{{ item.documento_favorecido }}</td>
+            <td class="py-2 px-4 border-b">{{ item.nome_favorecido }}</td>
+            <td class="py-2 px-4 border-b">{{ formatCurrency(item.valor) }}</td>
+          </tr>
+        </tbody>
+      </table>
+    </div>
+  </div>
+  <div class="flex justify-between mt-4">
+    <button class="bg-blue-500 text-white px-4 py-2 rounded" @click="prevPage" :disabled="currentPage === 1">
+      Anterior
+    </button>
+    <button class="bg-blue-500 text-white px-4 py-2 rounded" @click="nextPage" :disabled="currentPage === totalPages">
+      Próximo
+    </button>
+  </div>
 </template>
 
 <script setup>
@@ -68,7 +69,7 @@ const itemsTest = ref(testeData);
 const router = useRouter();
 const empenhoData = ref([]);
 const currentPage = ref(1);
-const rowsPerPage = 20;
+const rowsPerPage = 10;
 
 const paginatedData = computed(() => {
   const start = (currentPage.value - 1) * rowsPerPage;
